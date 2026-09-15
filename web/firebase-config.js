0c47f7bf3d7ef6a1ecee6e757c82681e8e2ef146
peterbebawy <115377883+peterbebawy@users.noreply.github.com>
2026-09-15 15:07:19 +0300

    Update firebase-config.js

--- web/firebase-config.js
+++ web/firebase-config.js
@@ -1,17 +1,9 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
+window.YALLA_FIREBASE_CONFIG = {
   apiKey: "AIzaSyBK-R8aFputGeaKpijvLt67fRjdGMSnByQ",
   authDomain: "yalla-toktok-da93c.firebaseapp.com",
+  databaseURL: "https://yalla-toktok-da93c-default-rtdb.firebaseio.com/",
   projectId: "yalla-toktok-da93c",
   storageBucket: "yalla-toktok-da93c.firebasestorage.app",
   messagingSenderId: "101346029105",
   appId: "1:101346029105:web:dbfd37f1f2de88341eaf10"
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
